89d9f128a7f903ed740102ffc7540c7fbdff4627
lukijzaw97 <35146082+lukijzaw97@users.noreply.github.com>
2018-02-09 14:44:56 +0100

    With Haddock documentation

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,37 @@
-module Main(main,mainWithMessages,mainWithoutMessages) where
+{-|
+Module      : Main
+Description : Module responsible for interaction with user.
+
+Module contains two versions of communication with user. The version should be estimated before running the project.
+The first option (by implication) is mainWithMessages, which enables communication and instrucing user.
+The second option is mainWithoutMessages, with this option you can work without neccessary coments.
+This option might be helpful when you are going to use piping in console.
+
+Some functions may use type Val = Int to differentiate values stored in SegmentTree from Int used to specify boundaries and size.
+-}
+
+module Main(main,mainWithMessages,mainWithoutMessages,interactWithUser) where
 
 import Data.String
 import Data.List
 
 import SegmentTree
-	--findSegmentTreeSize
-	--initialize,
-	--insertInitialNumbers,
-	--updateSetValueOnInterval,
-	--updateAddValueOnInterval,
-	--queryMinimumOnInterval,
-	--queryMaximumOnInterval,
-	--querySumeOnInterval,
-	--getPartOfSegmentTree,
-	--show',
-	--SegmentTree(..)
 
 
 
 type Val = Int
 
 
-
+-- | This function checks if interval given by user is correct.
 isIntervalIncorrect :: Int -> [Int] -> Bool
 isIntervalIncorrect _ x | ((length x) < 2) = True
 isIntervalIncorrect n (x1:x2:x3) = not (0 <= x1 && x1 <= x2 && x2 <= n)
 
 
-
-interactWithUser' :: Int -> SegmentTree -> IO ()
-interactWithUser' n = interactWithUser
-	where interactWithUser t1 = do
+-- | This function proceeds users' commands.
+interactWithUser :: Int -> SegmentTree -> IO ()
+interactWithUser n = interactWithUser'
+	where interactWithUser' t1 = do
 		input <- getLine
 		if (input == ":q" || input == "q" || input == "quit" || input == "exit")
 			then putStr ""
@@ -41,79 +42,79 @@ interactWithUser' n = interactWithUser
 				if (isIntervalIncorrect n args)
 					then do
 						putStrLn "Invalid interval - please ensure that interval is included in SegmentTree boundaries."
-						interactWithUser t1
+						interactWithUser' t1
 					else do
 						case beg of
 							"set" -> do
 								if ((length args) < 3)
 									then do
 										putStrLn "Too few arguments for set update."
-										interactWithUser t1
+										interactWithUser' t1
 									else do
 										let (a1:a2:a3:a4) = args
 										let t2 = updateSetValueOnInterval a1 a2 a3 t1
-										interactWithUser t2
+										interactWithUser' t2
 							"add" -> do
 								if ((length args) < 3)
 									then do
 										putStrLn "Too few arguments for add update."
-										interactWithUser t1
+										interactWithUser' t1
 									else do
 										let (a1:a2:a3:a4) = args
 										let t2 = updateAddValueOnInterval a1 a2 a3 t1
-										interactWithUser t2
+										interactWithUser' t2
 							"min" -> do
 								if ((length args) < 2)
 									then do
 										putStrLn "Too few arguments for min query."
-										interactWithUser t1
+										interactWithUser' t1
 									else do
 										let (a1:a2:a3) = args
 										let t2 = updateAddValueOnInterval a1 a2 0 t1
 										print (queryMinimumOnInterval a1 a2 t2)
-										interactWithUser t2
+										interactWithUser' t2
 							"max" -> do
 								if ((length args) < 2)
 									then do
 										putStrLn "Too few arguments for max query."
-										interactWithUser t1
+										interactWithUser' t1
 									else do
 										let (a1:a2:a3) = args
 										let t2 = updateAddValueOnInterval a1 a2 0 t1
 										print (queryMaximumOnInterval a1 a2 t2)
-										interactWithUser t2
+										interactWithUser' t2
 							"sum" -> do
 								if ((length args) < 2)
 									then do
 										putStrLn "Too few arguments for sum query."
-										interactWithUser t1
+										interactWithUser' t1
 									else do
 										let (a1:a2:a3) = args
 										let t2 = updateAddValueOnInterval a1 a2 0 t1
 										print (querySumOnInterval a1 a2 t2)
-										interactWithUser t2
+										interactWithUser' t2
 							"get" -> do
 								if ((length args) < 2)
 									then do
 										putStrLn "Too few arguments for tre query."
-										interactWithUser t1
+										interactWithUser' t1
 									else do
 										let (a1:a2:a3) = args
 										let l1 = getPartOfSegmentTree a1 a2 t1
 										print l1
-										interactWithUser t1
+										interactWithUser' t1
 							otherwise -> do
 								putStrLn "Invalid operation - check your operation and arguments."
-								interactWithUser t1
+								interactWithUser' t1
 
 
 
-
+-- | This is main function. In this function the appropriate version of cummication with user is called.
 main :: IO ()
 main = mainWithMessages
 
 
-
+-- | This function communicates with user and instructs them.
 mainWithMessages :: IO ()
 mainWithMessages = do
 		putStrLn "Please give one number: n - number of numbers stored in segment tree."
@@ -128,11 +129,11 @@ mainWithMessages = do
 		putStrLn ("Please introduce operations on intervals included in [0," ++ show n1 ++ ").")
 		putStrLn ("Remember that intervals are in format [beg,end), where beg >= 0 and end <= " ++ show n1 ++ ".")
 		putStrLn ("When you want to close program, type one of [q,:q,quit,exit] and press enter.")
-		interactWithUser' n1 t1
+		interactWithUser n1 t1
 		putStrLn "See you next time!"
 
 
-
+-- | This function communicates with user without instruction.
 mainWithoutMessages :: IO ()
 mainWithoutMessages = do
 	x <- getLine
@@ -142,4 +143,4 @@ mainWithoutMessages = do
 	let initials = map read (words input) :: [Val]
 	let t0 = initialize 0 n
 	let t1 = insertInitialNumbers 0 n initials t0
-	interactWithUser' n1 t1
+	interactWithUser n1 t1
--- src/SegmentTree.hs
+++ src/SegmentTree.hs
@@ -1,4 +1,27 @@
+{-|
+Module      : SegmentTree
+Description : Module delivering complete structure of semgent tree with useful operations.
+
+Module delivers complete structure of semgent tree with listed below useful operations:
+
+set b e v - change values on interval [b,e) into v
+
+add b e v - add value v to all numbers on interval [b,e)
+
+sum b e - return sum of numbers on interval [b,e)
+
+min b e - return minimum number on interval [b,e)
+
+max b e - return maximum number on interval [b,e)
+
+get b e - print numbers on interval [b,e) in list format
+
+Some functions may use type Val = Int to differentiate values stored in SegmentTree from Int used to specify boundaries and size.
+-}
+
+
 module SegmentTree (
+	SegmentTree(..),
 	findSegmentTreeSize,
 	initialize,
 	insertInitialNumbers,
@@ -8,83 +31,106 @@ module SegmentTree (
 	queryMaximumOnInterval,
 	querySumOnInterval,
 	getPartOfSegmentTree,
-	show',
-	SegmentTree(..)
+	zero,
+	minusInf,
+	plusInf,
+	getLeftChild,
+	getRightChild,
+	getLeftBound,
+	getRightBound,
+	getMinimum,
+	getMaximum,
+	getSume,
+	getModifierOfSum,
+	getModifierOfEquality
 ) where
 
 import Data.Int
 import Data.Bool
 
+
 type Val = Int
 
---SegmentTree = nullLeaf | Node leftChild rightChild leftBound rightBound minimum maximum sume modifier
-data SegmentTree = Null | Node SegmentTree SegmentTree Int Int Val Val Val Val Bool deriving Show
+-- | SegmentTree is either NullLeaf or Node containing two children, own boundaries and values to compute answers for queries effectively
+data SegmentTree = Null -- ^ NullLeaf
+								  | Node SegmentTree SegmentTree Int Int Val Val Val Val Bool -- ^ Node leftChild rightChild leftBound rightBound minimum maximum sume (modifier of sum) (modifier of equality)
 
 
 
---Functions used to find optimal size of segment tree - optimal is in case of power of two
 
-findPowerOfTwo :: Int -> Int -> Int
-findPowerOfTwo s n | (s >= n) = s
-findPowerOfTwo s n = findPowerOfTwo (2 * s) n
-
+-- | This function returns optimal size of segment tree - optimal is in case of power of two.
 findSegmentTreeSize :: Int -> Int
 findSegmentTreeSize n = findPowerOfTwo 1 n
+		where
+			findPowerOfTwo s n | (s >= n) = s
+			findPowerOfTwo s n = findPowerOfTwo (2 * s) n
 
 
 
---Constant functions returning neutral values for operations on SegmentTree
 
+-- Constant functions which return neutral values for operations on SegmentTree.
+
+
+-- | This function returns 'zero' of operations on SegmentTree
 zero :: Val
 zero = 0
 
+-- | This function returns minimum value (minus infinity) of operations on SegmentTree
 minusInf :: Val
 minusInf = (-1) * 2^63
 
+-- | This function returns maximum value (plus infinity) of operations on SegmentTree
 plusInf :: Val
 plusInf = 2^63 - 1
 
 
 
---These functions return values stored in SegmentTree (similar to getters in Java)
 
+-- These functions return values stored in SegmentTree (similar to getters).
+
+-- | This function returns LeftChild of specified Node of SegmentTree
 getLeftChild :: SegmentTree -> SegmentTree
 getLeftChild t @ (Node lef _ _ _ _ _ _ _ _)	= lef
 
+-- | This function returns RightChild of specified Node of SegmentTree
 getRightChild :: SegmentTree -> SegmentTree
 getRightChild t @ (Node _ rig _ _ _ _ _ _ _) = rig
 
+-- | This function returns LeftBound of specified Node of SegmentTree
 getLeftBound :: SegmentTree -> Int
 getLeftBound t @ (Node _ _ b _ _ _ _ _ _) = b
 
+-- | This function returns RightBound of specified Node of SegmentTree
 getRightBound :: SegmentTree -> Int
 getRightBound t @ (Node _ _ _ e _ _ _ _ _) = e
 
+-- | This function returns minimum on interval of specified Node of SegmentTree
 getMinimum :: SegmentTree -> Val
 getMinimum t @ (Node _ _ _ _ min _ _ _ _) = min
 
+-- | This function returns maximum on interval of specified Node of SegmentTree
 getMaximum :: SegmentTree -> Val
 getMaximum t @ (Node _ _ _ _ _ max _ _ _) = max
 
+-- | This function returns sume of values on interval of specified Node of SegmentTree
 getSume :: SegmentTree -> Val
 getSume t @ (Node _ _ _ _ _ _ sum _ _) = sum
 
-getModifier :: SegmentTree -> Val
-getModifier t @ (Node _ _ _ _ _ _ _ mod _) = mod
+-- | This function returns modifier of sume of values on interval of specified Node of SegmentTree
+getModifierOfSum :: SegmentTree -> Val
+getModifierOfSum t @ (Node _ _ _ _ _ _ _ mod _) = mod
 
-getEqualAll :: SegmentTree -> Bool
-getEqualAll t @ (Node _ _ _ _ _ _ _ _ equ) = equ
+-- | This function returns modifier of equality of values on interval of specified Node of SegmentTree
+getModifierOfEquality :: SegmentTree -> Bool
+getModifierOfEquality t @ (Node _ _ _ _ _ _ _ _ equ) = equ
 
 
 
---Operations which pass on modifiers from node to its children - modifier of sum and modifier of equality
 
+-- | This function pushes modifier of sum from node to its children.
 pushModifierOfSumToChild :: Val -> SegmentTree -> SegmentTree
-
 pushModifierOfSumToChild _ Null = Null
-
 pushModifierOfSumToChild modifier t | (modifier == zero) = t
-
 pushModifierOfSumToChild modifier (Node lef rig b e min max sum mod equ) = let
 	newMin = min + modifier
 	newMax = max + modifier
@@ -93,13 +139,10 @@ pushModifierOfSumToChild modifier (Node lef rig b e min max sum mod equ) = let
 	in (Node lef rig b e newMin newMax newSum newMod equ)
 
 
-
+-- | This function pushes modifier of equality from node to its children.
 pushModifierOfEqualityToChild :: Bool -> Val -> SegmentTree -> SegmentTree
-
 pushModifierOfEqualityToChild equal _ t | (equal == False) = t
-
 pushModifierOfEqualityToChild _ _ Null = Null
-
 pushModifierOfEqualityToChild _ value (Node lef rig b e min max sum mod equ) = let
 	newMin = value
 	newMax = value
@@ -109,11 +152,9 @@ pushModifierOfEqualityToChild _ value (Node lef rig b e min max sum mod equ) = l
 	in (Node lef rig b e newMin newMax newSum newMod newEqu)
 
 
-
+-- | This function pushes modifiers from node to its children - modifier of sum and modifier of equality.
 pushModifiersToChildren :: SegmentTree -> SegmentTree
-
 pushModifiersToChildren Null = Null
-
 pushModifiersToChildren t @ (Node lef rig b e min max sum mod equ) = let
 	lef1 = pushModifierOfSumToChild mod lef
 	rig1 = pushModifierOfSumToChild mod rig
@@ -126,8 +167,7 @@ pushModifiersToChildren t @ (Node lef rig b e min max sum mod equ) = let
 
 
 
---Operations executed on segment tree - all of them have O(log n) complexity, where n = size of tree
-
+-- | This function creates SegmentTree of specified size.
 initialize :: Int -> Int -> SegmentTree
 initialize l r =
 	if (r - l <= 1)
@@ -140,7 +180,7 @@ initialize l r =
 			m	= (l + r) `div` 2
 
 
-
+-- | This function inserts some values into SegmentTree, values are given from beginning (left).
 insertInitialNumbers :: Int -> Int -> [Val] -> SegmentTree -> SegmentTree
 insertInitialNumbers _ _ [] t = t
 insertInitialNumbers ind n _ t | (ind >= n) = t
@@ -149,21 +189,27 @@ insertInitialNumbers ind n (x:xs) t =
 
 
 
+
+-- Operations executed on segment tree - all of them have O(log n) complexity, where n = size of tree.
+-- In updates the structure is changed and due to it new structure is returned.
+
+
+-- | This function updates values (adds given v to all values) on specified inverval in SegmentTree
 updateAddValueOnInterval :: Int -> Int -> Val -> SegmentTree -> SegmentTree
 updateAddValueOnInterval l r dif = updateAddValueOnInterval'
 		where
 			updateAddValueOnInterval' Null = Null
 
-			updateAddValueOnInterval' t @ (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = t					--Intervals [l,r) and [b,e) do not intersect
+			updateAddValueOnInterval' t @ (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = t                   --Intervals [l,r) and [b,e) do not intersect
 
-			updateAddValueOnInterval' (Node lef rig b e min max sum mod equ) | (l <= b && e <= r) = let		--Interval [b,e) is included in interval [l,r)
+			updateAddValueOnInterval' (Node lef rig b e min max sum mod equ) | (l <= b && e <= r) = let       --Interval [b,e) is included in interval [l,r)
 				newMin = min + dif
 				newMax = max + dif
 				newMod = mod + dif
 				newSum = sum + (e - b) * dif
 				in (Node lef rig b e newMin newMax newSum newMod equ)
 
-			updateAddValueOnInterval' t @ (Node lef rig b e min max sum mod equ) = let			--[l,r) and [b,e) intersect and all values on [b,e) are equal
+			updateAddValueOnInterval' t @ (Node lef rig b e min max sum mod equ) = let                        --[l,r) and [b,e) intersect and all values on [b,e) are equal
 				t1 = pushModifiersToChildren t
 				lef1 = getLeftChild t1
 				rig1 = getRightChild t1
@@ -178,14 +224,15 @@ updateAddValueOnInterval l r dif = updateAddValueOnInterval'
 
 
 
+-- | This function updates values (changes all values into given v) on specified inverval in SegmentTree
 updateSetValueOnInterval :: Int -> Int -> Val -> SegmentTree -> SegmentTree
 updateSetValueOnInterval l r value = updateSetValueOnInterval'
 		where
 			updateSetValueOnInterval' Null = Null
 
-			updateSetValueOnInterval' t @ (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = t					--Intervals [l,r) and [b,e) do not intersect
+			updateSetValueOnInterval' t @ (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = t                   --Intervals [l,r) and [b,e) do not intersect
 
-			updateSetValueOnInterval' (Node lef rig b e min max sum mod equ) | (l <= b && e <= r) = let		--Interval [b,e) is included in interval [l,r)
+			updateSetValueOnInterval' (Node lef rig b e min max sum mod equ) | (l <= b && e <= r) = let       --Interval [b,e) is included in interval [l,r)
 				newMin = value
 				newMax = value
 				newSum = (e - b) * value
@@ -193,7 +240,7 @@ updateSetValueOnInterval l r value = updateSetValueOnInterval'
 				newEqu = True
 				in (Node lef rig b e newMin newMax newSum newMod newEqu)
 
-			updateSetValueOnInterval' t @ (Node lef rig b e min max sum mod equ) = let			--[l,r) and [b,e) intersect and all values on [b,e) are equal
+			updateSetValueOnInterval' t @ (Node lef rig b e min max sum mod equ) = let                        --[l,r) and [b,e) intersect and all values on [b,e) are equal
 				t1 = pushModifiersToChildren t
 				lef1 = getLeftChild t1
 				rig1 = getRightChild t1
@@ -207,63 +254,74 @@ updateSetValueOnInterval l r value = updateSetValueOnInterval'
 				in (Node newLef newRig b e newMin newMax newSum newMod newEqu)
 
 
---In queries it is assumed that modifiers have been pushed to children before calling a function.
 
+
+-- In queries it is assumed that modifiers have been pushed to children before calling the function.
+
+
+-- | This function returns maximum value on specified inverval in SegmentTree
 queryMinimumOnInterval :: Int -> Int -> SegmentTree -> Val
 queryMinimumOnInterval l r = queryMinimumOnInterval'
 		where
 			queryMinimumOnInterval' Null = plusInf
 
-			queryMinimumOnInterval' (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = plusInf				--Intervals [l,r) and [b,e) do not intersect
+			queryMinimumOnInterval' (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = plusInf                   --Intervals [l,r) and [b,e) do not intersect
 
-			queryMinimumOnInterval' (Node _ _ b e min _ _ _ _) | (l <= b && e <= r) = min				--Interval [b,e) is included in interval [l,r)
+			queryMinimumOnInterval' (Node _ _ b e min _ _ _ _) | (l <= b && e <= r) = min                     --Interval [b,e) is included in interval [l,r)
 
-			queryMinimumOnInterval' (Node lef rig _ _ _ _ _ _ _) = 								--[l,r) and [b,e) intersect and all values on [b,e) are equal
+			queryMinimumOnInterval' (Node lef rig _ _ _ _ _ _ _) =                                            --[l,r) and [b,e) intersect and all values on [b,e) are equal
 				minimum [(queryMinimumOnInterval l r lef), (queryMinimumOnInterval l r rig)]
 
 
 
+-- | This function returns minimum value on specified inverval in SegmentTree
 queryMaximumOnInterval :: Int -> Int -> SegmentTree -> Val
 queryMaximumOnInterval l r = queryMaximumOnInterval'
 		where
 			queryMaximumOnInterval' Null = minusInf
 
-			queryMaximumOnInterval' (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = minusInf			--Intervals [l,r) and [b,e) do not intersect
+			queryMaximumOnInterval' (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = minusInf                  --Intervals [l,r) and [b,e) do not intersect
 
-			queryMaximumOnInterval' (Node _ _ b e _ max _ _ _) | (l <= b && e <= r) = max				--Interval [b,e) is included in interval [l,r)
+			queryMaximumOnInterval' (Node _ _ b e _ max _ _ _) | (l <= b && e <= r) = max                     --Interval [b,e) is included in interval [l,r)
 
-			queryMaximumOnInterval' (Node lef rig _ _ _ _ _ _ _) = 								--[l,r) and [b,e) intersect and all values on [b,e) are equal
+			queryMaximumOnInterval' (Node lef rig _ _ _ _ _ _ _) =                                            --[l,r) and [b,e) intersect and all values on [b,e) are equal
 				maximum [(queryMaximumOnInterval l r lef), (queryMaximumOnInterval l r rig)]
 
 
 
+-- | This function returns sume of values on specified inverval in SegmentTree
 querySumOnInterval :: Int -> Int -> SegmentTree -> Val
 querySumOnInterval l r = querySumOnInterval'
 		where
 			querySumOnInterval' Null = zero
 
-			querySumOnInterval' (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = zero				--Intervals [l,r) and [b,e) do not intersect
+			querySumOnInterval' (Node _ _ b e _ _ _ _ _) | (e <= l || r <= b) = zero                          --Intervals [l,r) and [b,e) do not intersect
 
-			querySumOnInterval' (Node _ _ b e _ _ sum _ _) | (l <= b && e <= r) = sum				--Interval [b,e) is included in interval [l,r)
+			querySumOnInterval' (Node _ _ b e _ _ sum _ _) | (l <= b && e <= r) = sum                         --Interval [b,e) is included in interval [l,r)
 
-			querySumOnInterval' (Node lef rig _ _ _ _ _ _ _) = 								--[l,r) and [b,e) intersect and all values on [b,e) are equal
+			querySumOnInterval' (Node lef rig _ _ _ _ _ _ _) =                                                --[l,r) and [b,e) intersect and all values on [b,e) are equal
 				(querySumOnInterval l r lef) + (querySumOnInterval l r rig)
 
 
 
---With these functions the values stored in SegmentTree can be printed in sensible way
 
+
+-- | With this function the values stored in SegmentTree or its structure can be printed in sensible way.
 getPartOfSegmentTree :: Int -> Int -> SegmentTree -> [Val]
 getPartOfSegmentTree b e t = do
 	[(queryMaximumOnInterval i (i+1) (updateAddValueOnInterval i (i+1) zero t))  | i <- [b..(e-1)]]
 
 
-show'' :: SegmentTree -> String
-show'' Null = "Null"
-show'' (Node lef rig b e min max sum mod equ) =
+
+-- | This function enables to convert structure of SegmentTree to String in clear way.
+show' :: SegmentTree -> String
+show' Null = "Null"
+show' (Node lef rig b e min max sum mod equ) =
 	"Node " ++ show b ++ " " ++ show e ++ "  " ++ show min ++ " " ++ show max ++ " " ++ show sum ++ "  " ++ show mod
-	++ " " ++ show equ	++ "\n" ++ show'' lef ++ "\n" ++ show'' rig
+	++ " " ++ show equ	++ "\n" ++ show' lef ++ "\n" ++ show' rig
 
 
-show' :: SegmentTree -> IO ()
-show' t = putStrLn (show'' t)
+
+-- | This instance enables to write out structure of SegmentTree in clear way.
+instance Show (SegmentTree) where
+	show (t) = show' t
